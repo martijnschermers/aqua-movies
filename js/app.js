78f8ceb45ebdf891fc2165d67f091b1c441a5895
Martoen <martijnschermers2@gmail.com>
2021-10-13 17:37:43 +0200

    Site finally works correctly on mobile

--- js/app.js
+++ js/app.js
@@ -26,51 +26,29 @@ function search() {
   var searchterm = document.getElementById('inputSearch').value;
 
   if (searchterm !== "") {
-    console.log('zoekterm = ' + searchterm);
-
     let themovies = document.getElementById('frontpage-movies')
-
-    console.log("Lijst van filmtitels:");
-    console.log(themovies);
-    
-    const titles = themovies.querySelectorAll(".card-title");
-    console.log(titles)
-    Array.from(titles).forEach(function (title) {
-      console.log(title.textContent); 
-    });
 
     let searchResults = [];
 
-    console.log("Filter de lijst:");
     Array.from(themovies.children).forEach(function (movie) {
       const movietitle = movie.querySelectorAll(".card-title");
 
-
       Array.from(movietitle).forEach(function (title) {
-        console.log(title.textContent.toLowerCase());
         let found = title.textContent.toLowerCase().includes(searchterm.toLowerCase());
         if (found) {
-          console.log("Yes, gevonden: " + title.textContent);
           searchResults.push(movie);
         }
       });
-
     });
 
     if (searchResults.length > 0) {
-      console.log(`Er zijn ${searchResults.length} resultaten`);
-
       themovies.innerHTML = ``;
       searchResults.forEach(function (movie) {
         themovies.innerHTML = themovies.innerHTML.concat('<div class="col-md-4">' + movie.innerHTML + "</div>");
       });
-
-
     } else {
-      console.log("Helaas niet gevonden");
       themovies.innerHTML = `<p>Er zijn geen films die aan je zoekterm voldoen.</p>`;
     }
-
     document.getElementById("inputSearch").value = "";
   }
 }
