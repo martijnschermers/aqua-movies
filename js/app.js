4f3f58987c31746a27d21b2adb1aff7264f756d4
Martoen <martijnschermers2@gmail.com>
2021-10-15 12:11:07 +0200

    General cleanings

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-function readMore() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Lees meer";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Lees minder";
-    moreText.style.display = "inline";
-  }
-}
-
 $('.carousel').carousel({
   interval: 3500,
   keyboard: true,
@@ -52,5 +36,3 @@ function search() {
     document.getElementById("inputSearch").value = "";
   }
 }
-
-
